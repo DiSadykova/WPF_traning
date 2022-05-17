f2f18675cf325bee5059439c02369d952235c175
DiSadykova <dilyara.mozhet.sadykova@gmail.com>
2022-05-17 08:28:20 +0300

    repos sort

---
+++ WpfLab_14_2/WpfLab_14_2/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLab_14_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                Name = "Хлеб",
+                Price = 43.4,
+                ImageSource = "Data/hleb.jpg",
+                Category = Categories.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Мясо",
+                Price = 43.50,
+                ImageSource = "Data/myaso.jpg",
+                Category = Categories.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Чайник",
+                Price = 1450.00,
+                ImageSource = "Data/chaynik.jpg",
+                Category = Categories.DomesticAppliances
+            });
+            products.Add(new Product()
+            {
+                Name = "Блендер",
+                Price = 4140.50,
+                ImageSource = "Data/blender.jpg",
+                Category = Categories.DomesticAppliances
+            });
+            lstBox.ItemsSource = products;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            products.Add(new Product()
+            {
+                Name = "Яблоки",
+                Price = 104.50,
+                ImageSource = "Data/yabloki.jpg",
+                Category = Categories.Food
+            });
+        }
+    }
+}
---
+++ WpfLab_5_2/WpfLab_5_2_2/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Drawing;
+
+namespace WpfLab_5_2_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color=Colors.Red;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Orange;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Yellow;
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Magenta;
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+
+        //private void Button_Click_10(object sender, RoutedEventArgs e)
+        //{
+        //    inkCanvas.DefaultDrawingAttributes.Color = ;
+        //}
+
+        //private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        // {
+        //     SaveFileDialog saveFileDialog = new SaveFileDialog();
+        //     if (saveFileDialog.ShowDialog() == true)
+        //     {
+        //        File. (saveFileDialog.FileName, inkCanvas.Strokes);
+        //     }
+        // }
+    }
+}
